e55a484d19938a255b9ed2da60a4607f83f0398e
Mark Heinis <heinis.mark@gmail.com>
2017-07-25 09:35:36 +0200

    dew point is not so intersting to most of the users so I suggest that would be shown last.

--- NL.HNOGames.Domoticz/NL.HNOGames.Domoticz/Models/DevicesModel.cs
+++ NL.HNOGames.Domoticz/NL.HNOGames.Domoticz/Models/DevicesModel.cs
@@ -179,24 +179,28 @@ namespace NL.HNOGames.Domoticz.Models
                     dataText = AppResources.direction + ": " + this.Direction + " " + this.DirectionStr;
                 if (!String.IsNullOrEmpty(this.ForecastStr))
                     dataText = this.ForecastStr;
+
+                if (this.Temp.HasValue)
+                    dataText += !String.IsNullOrEmpty(dataText) ? ", " + AppResources.temp + ": " + this.Temp + " " + App.getServerConfig().TempSign : AppResources.temp + ": " + this.Temp + " " + App.getServerConfig().TempSign;
+                if (!String.IsNullOrEmpty(this.HumidityStatus))
+                    dataText += !String.IsNullOrEmpty(dataText) ? ", " + AppResources.humidity + ": " + this.HumidityStatus : AppResources.humidity + ": " + this.HumidityStatus;
+
                 if (!String.IsNullOrEmpty(this.Speed))
                     dataText += !String.IsNullOrEmpty(dataText) ? ", " + AppResources.speed + ": " + this.Speed + " " + App.getServerConfig().WindSign : AppResources.speed + ": " + this.Speed + " " + App.getServerConfig().WindSign;
                 if (!String.IsNullOrEmpty(this.DewPoint))
                     dataText += !String.IsNullOrEmpty(dataText) ? ", " + AppResources.dewPoint + ": " + this.DewPoint + " " + App.getServerConfig().TempSign : AppResources.dewPoint + ": " + this.DewPoint + " " + App.getServerConfig().TempSign;
-                if (this.Temp.HasValue)
-                    dataText += !String.IsNullOrEmpty(dataText) ? ", " + AppResources.temp + ": " + this.Temp + " " + App.getServerConfig().TempSign : AppResources.temp + ": " + this.Temp + " " + App.getServerConfig().TempSign;
                 if (this.Barometer.HasValue)
                     dataText += !String.IsNullOrEmpty(dataText) ? ", " + AppResources.pressure + ": " + this.Barometer : AppResources.pressure + ": " + this.Barometer;
                 if (this.Chill.HasValue)
                     dataText += !String.IsNullOrEmpty(dataText) ? ", " + AppResources.chill + ": " + this.Chill + " " + App.getServerConfig().TempSign : AppResources.chill + ": " + this.Chill + " " + App.getServerConfig().TempSign;
-                if (!String.IsNullOrEmpty(this.HumidityStatus))
-                    dataText += !String.IsNullOrEmpty(dataText) ? ", " + AppResources.humidity + ": " + this.HumidityStatus : AppResources.humidity + ": " + this.HumidityStatus;
+                
                 if (!String.IsNullOrEmpty(this.Rain))
                     dataText = AppResources.rain + ": " + this.Rain + " mm";
                 if (!String.IsNullOrEmpty(this.RainRate))
                     dataText += ", " + AppResources.rainrate + ": " + this.RainRate + " mm/h";
                 if (String.IsNullOrEmpty(dataText))
                     dataText = Data;
+
                 return String.Format("{0}: {1}", AppResources.status, dataText);
             }
         }

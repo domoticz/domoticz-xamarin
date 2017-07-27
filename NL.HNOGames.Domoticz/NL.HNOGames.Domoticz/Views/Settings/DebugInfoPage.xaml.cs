96c376dc489bb50e760c4282d873c0441d85c8a9
Mark Heinis <heinis.mark@gmail.com>
2017-07-27 15:26:54 +0200

    Add possibility to share the debug info

--- NL.HNOGames.Domoticz/NL.HNOGames.Domoticz/Views/Settings/DebugInfoPage.xaml.cs
+++ NL.HNOGames.Domoticz/NL.HNOGames.Domoticz/Views/Settings/DebugInfoPage.xaml.cs
@@ -4,6 +4,8 @@ using NL.HNOGames.Domoticz.ViewModels;
 using Xamarin.Forms;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using Plugin.Share;
+using NL.HNOGames.Domoticz.Resources;
 
 namespace NL.HNOGames.Domoticz.Views.Settings
 {
@@ -31,6 +33,15 @@ namespace NL.HNOGames.Domoticz.Views.Settings
             {
                 App.AppSettings.EnableJSONDebugging = swEnableJSONDebugging.IsToggled;
             };
+        }
+
+        private async Task ToolbarItem_Activated(object sender, EventArgs e)
+        {
+            //App.AppSettings.EnableDebugging, AppResources.category_debug + " Domoticz"
+            Plugin.Share.Abstractions.ShareMessage oMessage = new Plugin.Share.Abstractions.ShareMessage();
+            oMessage.Text = App.AppSettings.DebugInfo;
+            oMessage.Title = AppResources.category_debug + "- Domoticz";
+            await CrossShare.Current.Share(oMessage);
         }
     }
 }

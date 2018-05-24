e743156b214eaabde042133e4bcbc1f92e5bc9b8
HNO Mobile <heinis.mark@gmail.com>
2018-05-24 19:59:29 +0200

    Reworked the selector switch due to lateste beta changes of Domoticz
    Reworked the selector switch due to lateste beta changes of Domoticz

--- NL.HNOGames.Domoticz/NL.HNOGames.Domoticz/Helpers/UsefulBits.cs
+++ NL.HNOGames.Domoticz/NL.HNOGames.Domoticz/Helpers/UsefulBits.cs
@@ -1,31 +1,61 @@
 ﻿using Plugin.DeviceInfo;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace NL.HNOGames.Domoticz.Helpers
 {
-    public static class UsefulBits
-    {
-        public static bool IsNumeric(this string s)
-        {
-            float output;
-            return float.TryParse(s, out output);
-        }
+   public static class UsefulBits
+   {
+      /// <summary>
+      /// check if is numeric
+      /// </summary>
+      public static bool IsNumeric(this string s)
+      {
+         return float.TryParse(s, out float output);
+      }
 
-        public static String GetDeviceID()
-        {
-            return CrossDeviceInfo.Current.Id;
-        }
+      /// <summary>
+      /// Get device id
+      /// </summary>
+      /// <returns></returns>
+      public static String GetDeviceID()
+      {
+         return CrossDeviceInfo.Current.Id;
+      }
 
-        public static String GetMD5String(String input)
-        {
-            if (string.IsNullOrEmpty(input))
-                return null;
+      /// <summary>
+      /// Get the MD5 string
+      /// </summary>
+      public static String GetMD5String(string input)
+      {
+         if (string.IsNullOrEmpty(input))
+            return null;
 
-            return XLabs.Cryptography.MD5.GetMd5String(input);
-        }
-    }
+         return XLabs.Cryptography.MD5.GetMd5String(input);
+      }
+
+      /// <summary>
+      /// Check if a string is base64 encoded
+      /// </summary>
+      /// <param name="input"></param>
+      /// <returns></returns>
+      public static bool IsBase64Encoded(string input)
+      {
+         if (string.IsNullOrEmpty(input))
+            return false;
+         return Regex.Match(input, "^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$").Success;
+      }
+
+      /// <summary>
+      /// Decode base 64 string
+      /// </summary>
+      public static string DecodeBase64String(string input)
+      {
+         if (String.IsNullOrEmpty(input))
+            return null;
+         byte[] plain = Convert.FromBase64String(input);
+         return (Encoding.GetEncoding("UTF-8")).GetString(plain);
+      }
+   }
 }

2da3b659fc5a8d2213087f9bf9391813140898da
HNO Mobile <heinis.mark@gmail.com>
2019-04-14 19:48:33 +0200

    Various layout fixes

--- NL.HNOGames.Domoticz/NL.HNOGames.Domoticz/Controls/CustomTabbedPage.cs
+++ NL.HNOGames.Domoticz/NL.HNOGames.Domoticz/Controls/CustomTabbedPage.cs
@@ -4,34 +4,14 @@ using Xamarin.Forms.Internals;
 
 namespace NL.HNOGames.Domoticz.Controls
 {
-   /// <summary>
-   /// Custom tabbed page object
-   /// </summary>
    [Preserve(AllMembers = true)]
    public class CustomTabbedPage : TabbedPage
    {
-      #region Constructor & Destructor
-
-      /// <summary>
-      /// Default constructor
-      /// </summary>
       public CustomTabbedPage()
       {
-         //if (Device.RuntimePlatform == Device.iOS)
-         //   NavigationPage.SetHasNavigationBar(this, false);
-         //else if (Device.RuntimePlatform == Device.Android)
-            CurrentPageChanged += OnCurrentPageChanged;
+         CurrentPageChanged += OnCurrentPageChanged;
       }
 
-      #endregion
-
-      #region Private
-
-      /// <summary>
-      /// Set title of the 'current page' as the bottom tabbed page title
-      /// </summary>
-      /// <param name="sender">Sender object</param>
-      /// <param name="e">Event arguments</param>
       private void OnCurrentPageChanged(object sender, EventArgs e)
       {
          if (CurrentPage == null)
@@ -40,7 +20,5 @@ namespace NL.HNOGames.Domoticz.Controls
          if (titleView != null)
             NavigationPage.SetTitleView(this, titleView);
       }
-
-      #endregion
    }
 }
